e2cdd176170b2c1d0447cefc43bfcbafca584155
brnt-toast <yoyo.beerme@gmail.com>
2023-04-12 00:57:17 -0400

    refactor

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "../style.css"
+    import NavBar from "../components/NavBar.svelte";
+</script>
+
+<NavBar />
+
+<slot></slot>
--- src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    import "../style.css"
-    import NavBar from "../components/NavBar.svelte";
-</script>
-
-<NavBar />
